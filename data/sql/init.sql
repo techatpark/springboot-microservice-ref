104864758225c917b7a035cdc5fcf86f4ff3961f
sathishk <sathishkumar.thiyagarajan@gmail.com>
2019-10-06 19:34:38 +0800

    JDBC Template added

--- data/sql/init.sql
+++ data/sql/init.sql
@@ -1,1 +1,7 @@
-create table sometable(id int);
+DROP TABLE IF EXISTS commment;
+
+CREATE TABLE commment (
+    id Bigserial PRIMARY KEY NOT NULL,
+    message varchar(100) NOT NULL,
+    topic varchar(100) NOT NULL
+);
