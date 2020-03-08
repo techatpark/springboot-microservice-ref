87b15b2e4f92bfd4aba52cc729dc047cef97250e
rmhari <rm.haripriya@gmail.com>
2020-03-08 18:23:23 +0530

    Create Database with Self Join

--- comment-service/src/main/resources/db/migration/V1__ddl.sql
+++ comment-service/src/main/resources/db/migration/V1__ddl.sql
@@ -1,7 +1,14 @@
-DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS comments;
 
-CREATE TABLE comment (
-    id Bigserial PRIMARY KEY NOT NULL,
-    message varchar(100) NOT NULL,
-    topic varchar(100) NOT NULL
+CREATE TABLE comments(
+id INT NOT NULL AUTO_INCREMENT,
+text VARCHAR (255),
+created_by VARCHAR (255),
+topic VARCHAR (255),
+comments_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (comments_id) REFERENCES comments(id),
+CONSTRAINT valid_root 
+        CHECK(topic is NOT NULL OR comments_id is NOT NULL)
+
 );
