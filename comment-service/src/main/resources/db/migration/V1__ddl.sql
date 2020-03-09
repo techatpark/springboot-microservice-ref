f236e1bc33a29c2d04a1d1af0cd45d707b324735
rmhari <rm.haripriya@gmail.com>
2020-03-09 11:04:45 +0530

    Created Index for created_by and topic

--- comment-service/src/main/resources/db/migration/V1__ddl.sql
+++ comment-service/src/main/resources/db/migration/V1__ddl.sql
@@ -1,14 +1,16 @@
 DROP TABLE IF EXISTS comments;
-
 CREATE TABLE comments(
-id INT NOT NULL AUTO_INCREMENT,
-text VARCHAR (255),
-created_by VARCHAR (255),
-topic VARCHAR (255),
-comments_id INT,
-PRIMARY KEY (id),
-FOREIGN KEY (comments_id) REFERENCES comments(id),
-CONSTRAINT valid_root 
-        CHECK(topic is NOT NULL OR comments_id is NOT NULL)
-
+  id INT NOT NULL AUTO_INCREMENT,
+  text VARCHAR (255),
+  created_by VARCHAR (255),
+  topic VARCHAR (255),
+  comments_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (comments_id) REFERENCES comments(id),
+  CONSTRAINT valid_root CHECK(
+    topic is NOT NULL
+    OR comments_id is NOT NULL
+  ),
+    INDEX user_index(created_by),
+    INDEX topic_index(topic)
 );
